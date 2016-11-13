8eb8d9cee5107c7463e99ff9827ce8e61f133923
Stine Bagger Holt <sbh@tm-group.com>
2016-11-13 18:08:13 +0100

    opgave 1

--- httpdemo/src/java/presentation/MyhttpServlet.java
+++ httpdemo/src/java/presentation/MyhttpServlet.java
@@ -34,6 +34,9 @@ public class MyhttpServlet extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         String name = request.getParameter("navn");
         String age = request.getParameter("alder");
+        
+        String imageurl = "http://www.tm-group.com/wp-content/uploads/2014/01/Stine-Bagger-Holt.jpg";
+        
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
@@ -42,8 +45,9 @@ public class MyhttpServlet extends HttpServlet {
             out.println("<title>Servlet MyhttpServlet</title>");            
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet MyhttpServlet at " + request.getContextPath() + "</h1>");
-            out.println("<h1>Hej "+name+" du er "+age+" år gammel </h1>");
+            out.println("<h1>Hej Mit navn er "+name+"</h1>");
+            out.println("<p>Jeg er "+age+" år gammel. Jeg arbejder til dagligt for TM-group med at implementere forskellige IT løsninger hos vores kunder, og er derfor igang med AU for at forbedre mine kompetencer. <br>Da jeg arbejder meget med databaser, netværk, webservices i mit daglige job er jeg ved at nå til et punkt jeg havde brug for at få basic på plads. <br> Tanken er at nå til at læse en diplom efter.<p>");
+            out.println("<img style=\"width: 200px\" src=\""+imageurl+"\"/>"); 
             out.println("</body>");
             out.println("</html>");
         }
